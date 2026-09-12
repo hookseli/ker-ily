85d5509b403a81058ee2065044257890841fa45f
hookseli <akseli.romppainen@hotmail.com>
2026-09-12 21:09:43 +0300

    w edit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'keraily-v1';
+const CACHE = 'keraily-v2';
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])));
@@ -14,6 +14,12 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
